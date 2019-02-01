11e48d0dcd00146ab191a2099c6496d40493eb18
Chaferfu <mathba@gmx.com>
2019-02-01 10:24:37 +0100

    Corpus des jouers ajouté dans le repertoire 'smashers'

--- corpus/script.py
+++ corpus/script.py
@@ -2,11 +2,17 @@ import os
 
 dejaVus = []
 truc = "References\n\n   Visible links"
+nbError = 0
 
 
 def go(nomFichier):
-	with open(nomFichier, "r") as f:
-		content = f.readlines()
+	try:
+		with open(nomFichier, "r") as f:
+			content = f.readlines()
+	except Exception:
+		print("Olalala" + nomFichier + "buggggg !!!!!")
+		nbError = nbError + 1
+		print("C'est l'erreur numero   " + str(nbError))
 		
 	for i in range(len(content)):
 		if 'Visible links' in content[i]:
@@ -19,7 +25,7 @@ def go(nomFichier):
 	for line in content[firstLink:]:
 		line = line.lstrip()
 		mots = line.split(" ")
-		if "." in mots[0] and "ssbwiki" in mots[1] and "4" not in mots[1] and ".png" not in mots[1] and ".jpg" not in mots[1] and ".php" not in mots[1] and "#" not in mots[1] and ".gif" not in mots[1]:
+		if "." in mots[0] and "ssbwiki" in mots[1] and "4" not in mots[1] and ".png" not in mots[1] and ".jpg" not in mots[1] and ".php" not in mots[1] and "#" not in mots[1] and ".gif" not in mots[1] and ("Smasher" in mots[1] or "Tournament" in mots[1]):
 			liens.append(mots[1].strip())
 	for lien in liens:
 		if lien not in dejaVus:
@@ -35,4 +41,4 @@ def go(nomFichier):
 			dejaVus.append(l)
 			go(l.replace("https://www.ssbwiki.com/", "").join(".txt"))
 
-go("ppu.txt")
+go("top.txt")
---
+++ scriptExp.py
@@ -0,0 +1,81 @@
+import os 
+
+dejaVus = []
+truc = "References\n\n   Visible links"
+# nbError = 0
+
+try:
+		with open("top.txt", "r") as f:
+			content = f.readlines()
+		
+		for i in range(len(content)):
+			if 'Visible links' in content[i]:
+				 firstLink = i+1
+				
+
+
+		liens = []
+
+		for line in content[firstLink:]:
+			line = line.lstrip()
+			mots = line.split(" ")
+			if "." in mots[0] and "ssbwiki" in mots[1] and "4" not in mots[1] and ".png" not in mots[1] and ".jpg" not in mots[1] and ".php" not in mots[1] and "#" not in mots[1] and ".gif" not in mots[1] and ("Smasher" in mots[1] or "Tournament" in mots[1]):
+				liens.append(mots[1].strip())
+		for lien in liens:
+			if lien not in dejaVus:
+				print("je lance lynx sur " + lien)
+				cmd = 'lynx "{0}" -dump -width=1024 > "{1}.txt"'.format(lien, lien.replace("https://www.ssbwiki.com/", ""))
+				# print(cmd)
+				os.system(cmd)
+				print("j'ai fini lynx")
+				
+		for l in liens:
+			if l not in dejaVus:
+				print("je lance go sur " + l)
+				dejaVus.append(l)
+				go(l.replace("https://www.ssbwiki.com/", "").join(".txt"))
+except Exception:
+		print("Olalala" + "toplsl" + "buggggg !!!!!")
+		# nbError = nbError + 1
+		# print("C'est l'erreur numero   " + str(nbError))
+
+
+def go(nomFichier):
+	if not os.path.isfile("./".join(nomFichier)): 
+		return
+
+	try:
+		with open(nomFichier, "r") as f:
+			content = f.readlines()
+		
+		for i in range(len(content)):
+			if 'Visible links' in content[i]:
+				 firstLink = i+1
+				
+
+
+		liens = []
+
+		for line in content[firstLink:]:
+			line = line.lstrip()
+			mots = line.split(" ")
+			if "." in mots[0] and "ssbwiki" in mots[1] and "4" not in mots[1] and ".png" not in mots[1] and ".jpg" not in mots[1] and ".php" not in mots[1] and "#" not in mots[1] and ".gif" not in mots[1] and ("Smasher" in mots[1] or "Tournament" in mots[1]):
+				liens.append(mots[1].strip())
+		for lien in liens:
+			if lien not in dejaVus:
+				print("je lance lynx sur " + lien)
+				cmd = 'lynx "{0}" -dump -width=1024 > "{1}.txt"'.format(lien, lien.replace("https://www.ssbwiki.com/", ""))
+				# print(cmd)
+				os.system(cmd)
+				print("j'ai fini lynx")
+				
+		for l in liens:
+			if l not in dejaVus:
+				print("je lance go sur " + l)
+				dejaVus.append(l)
+				go(l.replace("https://www.ssbwiki.com/", "").join(".txt"))
+	except Exception:
+		print("Olalala" + nomFichier + "buggggg !!!!!")
+		# nbError = nbError + 1
+		# print("C'est l'erreur numero   " + str(nbError))
+
