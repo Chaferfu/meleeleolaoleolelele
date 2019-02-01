384d1a44086c4eaa3f97e1f82439a99e7e342d33
kevinLacoste <k.b.h.lacoste@gmail.com>
2019-02-01 12:37:35 +0100

    Début d'extraction des pseudonymes des players (Incomplet)

--- dark/parsing.lua
+++ dark/parsing.lua
@@ -1,3 +1,23 @@
 local dark = require("dark")
 
-print(dark.version)
+local function preprocessing(seq)
+	--TODO : Séparer la ponctuation des mots (.,; etc.) et les espaces/sauts de ligne/tabulations superflues
+	end
+
+local P = dark.pipeline()
+seq = dark.sequence("Armada is a smasher from Sweden who mains Peach and Fox in Melee . Ken Hoang , also referred to as just Ken , SephirothKen , or The King of Smash , is an American professional smasher of Vietnamese descent who mains Marth , ever since his debut in Tournament Go 4 .")
+
+P:basic()
+P:lexicon("#character", "lexicon/ssbm_characters.txt");
+P:pattern([[ [#pseudo .] is /[^.]*/  smasher]])
+P(seq)
+
+
+local tags = {
+	["#character"] = "blue",
+	["#player"] = "green",
+	["#pseudo"] = "red"
+}
+
+print(seq:tostring(tags))
+
