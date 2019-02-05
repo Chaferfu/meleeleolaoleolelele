27d0c9fee286a6663fdf16543c3f054c5fc2822e
kevinLacoste <k.b.h.lacoste@gmail.com>
2019-02-05 17:42:44 +0100

    Debut d'extraction de pseudo(1), main(2), nationalité(3)

--- dark/parsing.lua
+++ dark/parsing.lua
@@ -5,19 +5,57 @@ local function preprocessing(seq)
 	end
 
 local P = dark.pipeline()
-seq = dark.sequence("Armada is a smasher from Sweden who mains Peach and Fox in Melee . Ken Hoang , also referred to as just Ken , SephirothKen , or The King of Smash , is an American professional smasher of Vietnamese descent who mains Marth , ever since his debut in Tournament Go 4 .")
-
+--[[seq = dark.sequence("Armada is a smasher from Sweden who mains Peach , Fox , Jigglypuff and Dr. Mario in Melee .")
+seq2 = dark.sequence("Ken Hoang , also referred to as just Ken , SephirothKen , or The King of Smash , is an American professional smasher of Vietnamese descent who mains Marth , ever since his debut in Tournament Go 4 .")
+seq3 = dark.sequence("FatGoku is a smasher from Oregon who is one of the top competitors in the Oregon/Pacific Northwest area.")
+seq4 = dark.sequence("Boyd is a Melee Ice Climbers , Fox , Falco and Mario main from Northeastern Ohio .")
+--]]
 P:basic()
-P:lexicon("#character", "lexicon/ssbm_characters.txt");
-P:pattern([[ [#pseudo .] is /[^.]*/  smasher]])
-P(seq)
+P:lexicon("#character", "lexicon/ssbm_characters.txt")
+P:pattern([[ [#pseudo .]  ("(".{0,30}? ")" | .{0,30}?)  is ([#nationality #W] | #w)*  ( smasher | Melee player )]])
+P:pattern([[ [#joueur #pseudo (#w | "," | "(" | ")"){0,40}? #character] ]])
+P:pattern([[ #pseudo (#w | "," | "(" | ")"){0,20}? fros[#nationality #W] ]])
+P:pattern([[ from [#nationality #W] ]])
+
+-- Detection des mains
+P:pattern([[ [#main #character] (("," [#main #character])*? and [#main #character])? main]])
+P:pattern([[ mains [#main #character] (("," [#main #character])*? and [#main #character])? ]])
+
+
 
 
 local tags = {
 	["#character"] = "blue",
 	["#player"] = "green",
-	["#pseudo"] = "red"
+	["#pseudo"] = "yellow",
+	["#joueur"] = "yellow",
+	["#nationality"] = "green",
+	["#main"] = "blue"
 }
 
-print(seq:tostring(tags))
+local rep = "../WikiSmash"
+for fichier in os.dir(rep) do
+	for line in io.lines(rep.."/"..fichier) do
+		line = line:gsub("%p", " %0 ")
+		seq = dark.sequence(line)
+		P(seq)
+		print(seq:tostring(tags))
+		print("\n")
+	end
+end
 
+--[[print(seq:tostring(tags))
+print("\n")
+
+P(seq2)
+print(seq2:tostring(tags))
+print("\n")
+
+P(seq3)
+print(seq3:tostring(tags))
+print("\n")
+
+P(seq4)
+print(seq4:tostring(tags))
+print("\n")
+--]]
