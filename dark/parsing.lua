019689b322325a0ed41b622d280fdf19f427a34a
kevinLacoste <k.b.h.lacoste@gmail.com>
2019-02-08 12:31:17 +0100

    Ajout d'une database (à completer)

--- dark/parsing.lua
+++ dark/parsing.lua
@@ -1,18 +1,30 @@
 local dark = require("dark")
 
-local function preprocessing(seq)
-	--TODO : Séparer la ponctuation des mots (.,; etc.) et les espaces/sauts de ligne/tabulations superflues
-	end
-
 local P = dark.pipeline()
---[[seq = dark.sequence("Armada is a smasher from Sweden who mains Peach , Fox , Jigglypuff and Dr. Mario in Melee .")
-seq2 = dark.sequence("Ken Hoang , also referred to as just Ken , SephirothKen , or The King of Smash , is an American professional smasher of Vietnamese descent who mains Marth , ever since his debut in Tournament Go 4 .")
-seq3 = dark.sequence("FatGoku is a smasher from Oregon who is one of the top competitors in the Oregon/Pacific Northwest area.")
-seq4 = dark.sequence("Boyd is a Melee Ice Climbers , Fox , Falco and Mario main from Northeastern Ohio .")
---]]
+
+local db = {
+	["players"] = {
+		
+	}, 
+	
+	["playerInfo"] = {
+		
+	}
+}
+
+--function registerindb (
+
+-- TODO Faire une premiere passe sur TOUS les joueurs pour enregistrer 
+-- les pseudo des joueurs independemment un lexique puis faire une 
+-- deuxieme passe en utilisant le lexique pour chercher tout le reste
+
 P:basic()
 P:lexicon("#character", "lexicon/ssbm_characters.txt")
+-- P:lexicon("#pseudo", "lexicon/pseudos.txt")
+
+-- Detection du pseudo du joueur
 P:pattern([[ [#pseudo .]  ("(".{0,30}? ")" | .{0,30}?)  is ([#nationality #W] | #w)*  ( smasher | Melee player )]])
+
 P:pattern([[ [#joueur #pseudo (#w | "," | "(" | ")"){0,40}? #character] ]])
 P:pattern([[ #pseudo (#w | "," | "(" | ")"){0,20}? from[#nationality #W] ]])
 P:pattern([[ from [#nationality #W] ]])
@@ -21,18 +33,33 @@ P:pattern([[ from [#nationality #W] ]])
 P:pattern([[ [#main #character] (("," [#main #character])*? and [#main #character])? main]])
 P:pattern([[ mains [#main #character] (("," [#main #character])*? and [#main #character])? ]])
 
--- Detection des surnoms du joueur (a completer)
+-- Detection des surnoms du joueur (a completer) (?)
 P:pattern([[ (also known as | aka | also referred to as) [#nickname .{1,10}?] ("," [#nickname .{1,10}?]){1,10}? and [#nickame .] ]])
+
+-- Detection des années
+P:pattern([[ [#year /^([1-2][0-9][0-9][0-9])$/] ]])
+
+-- Detection des tournois 
+P:pattern([[ ( won | winning )  ( [#tournoi .{0,5}?] ( "," | and | ".")){0,5} ]])
+P:pattern([[ facing .{0,3}? in .{0,2}? at [#tournoi .{0,5}?] ( "," | "." ) ]])
+
+-- Detection des phrases (attention aux acronymes)
+P:pattern([[ [#acronym (/^%u$/ ".")+ /^%u$/ ] ]])
+P:pattern([[ [#sentence /^[A-Z]/ (#acronym | .)*? "."] ]])
 
 local tags = {
 	["#character"] = "blue",
-	["#player"] = "green",
 	["#pseudo"] = "yellow",
 	["#joueur"] = "yellow",
 	["#nationality"] = "green",
 	["#main"] = "blue",
-	["#nickname"] = "green"
+	["#nickname"] = "green",
+	["#year"] = "yellow",
+	["#tournoi"] = "yellow",
 }
+
+seqList = {}
+i = 1;
 
 local rep = "../WikiSmash"
 for fichier in os.dir(rep) do
@@ -42,21 +69,11 @@ for fichier in os.dir(rep) do
 		P(seq)
 		print(seq:tostring(tags))
 		print("\n")
+		seqList[i] = seq
+		i = i + 1
 	end
 end
 
---[[print(seq:tostring(tags))
-print("\n")
-
-P(seq2)
-print(seq2:tostring(tags))
-print("\n")
-
-P(seq3)
-print(seq3:tostring(tags))
-print("\n")
-
-P(seq4)
-print(seq4:tostring(tags))
-print("\n")
---]]
+for i=1, #seqList, 1 do
+	print(seqList[i]:tostring(tags))
+end
