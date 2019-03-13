202056c80f0a2753ad2207ce1edc2ba431fd6559
Chaferfu <mathba@gmx.com>
2019-03-14 00:16:55 +0100

    fix

--- detectQuestion.lua
+++ detectQuestion.lua
@@ -267,7 +267,7 @@ main:pattern([[
 main:pattern([[
 
 	[#birthPlayerQuestion
-		/[Ww]hat/  (#w | #p){0,5}? (#player | "his" | "him" | "he" | "her" | "she")  (#w | #p){0,5}? ("birthday" | "bday" | "anniversary") "?"?
+		#questionWord  (#w | #p){0,5}? (#player | "his" | "him" | "he" | "her" | "she")  (#w | #p){0,5}? ("birthday" | "bday" | "anniversary") "?"?
 	]
 
 ]])
